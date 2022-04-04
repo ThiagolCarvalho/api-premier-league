eb33896ba21a4616a63e48ba02b8ce40ea0f7218
Thiago Carvalho <thiago.carvalho@pisomtech.com.br>
2022-04-04 19:42:03 -0300

    Aws cors error fix try

--- api/index.js
+++ api/index.js
@@ -5,7 +5,12 @@ const cors = require('cors')
 
 const router = require('./routes/premier-league/PremierLeagueRouter')
 
-app.use(cors())
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
 app.use('/api/premier-league', router)
 
 app.listen(config.get('api.port'), () => console.log('API is running on port ' + config.get('api.port')))
